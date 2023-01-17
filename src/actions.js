b5fcbd04c24b8ea150955aa6f7e7b7eee80df425
Richard <ricky.cary1@gmail.com>
2023-01-16 22:11:11 -0600

    REST routes created

--- src/actions.js
+++ src/actions.js
@@ -20,3 +20,27 @@ export const createAction = async ({ request }) => {
     })
     return redirect("/")
 }
+
+export const updateAction = async ({request, params}) => {
+    const formData = await request.formData()
+    const updatedCheese = {
+        name: formData.get("name"),
+        image: formData.get("image"),
+        countryOfOrigin: formData.get("countryOfOrigin")
+    }
+    await fetch(URL + "/cheese/" + params.id, {
+        method: "put",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(updatedCheese)
+    })
+    return redirect("/")
+}
+
+export const deleteAction = async ({params}) => {
+    await fetch(URL + "/cheese/" + params.id, {
+        method: "delete"
+    })
+    return redirect("/")
+}
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,13 +1,25 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, Form } from "react-router-dom"
 
 function Show(props) {
     const cheese = useLoaderData()
 
     return (
         <div className="cheese">
-            <h1>{cheese.name}</h1>
+            <h1>{cheese.name} Cheese</h1>
             <img src ={cheese.image} alt={cheese.name} />
             <h3>{cheese.countryOfOrigin}</h3>
+
+            <h2>Update {cheese.name} Cheese</h2>
+            <Form action={`/update/${cheese._id}`} method="post">
+                <input type="input" name="name" placeholder="Update Name"/>
+                <input type="input" name="image" placeholder="Cheese picture"/>
+                <input type="input" name="countryOfOrigin" placeholder="Location"/>
+                <input type="submit" value={`Update Cheese`} />
+            </Form>
+            <h2>Delete the Cheese</h2>
+            <Form action ={`/delete/${cheese._id}`} method="post">
+                <input type="submit" value={`Delete ${cheese.name}`} />
+            </Form>
         </div>
     )
   }
